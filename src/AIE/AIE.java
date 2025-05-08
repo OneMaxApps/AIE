bbaa92b03899733d1901e34f7600e464fe12730a
002 <002@192.168.42.44>
2025-05-08 18:32:21 +0300

    Optimized and added inner arrays for deeper encryption

--- src/AIE/AIE.java
+++ src/AIE/AIE.java
@@ -10,6 +10,7 @@ public final class AIE {
 	private AIE() {}
 	
 	private static final int MAX_DEEP = 200,
+							 MAX_DEEP_FOR_INNER_ARRAYS_OF_KEYS = 10,
 		 	 				 MAX_KEYS = 1000;
 	
 	private static final StringBuilder sb = new StringBuilder();
@@ -17,34 +18,47 @@ public final class AIE {
 	private static final SecureRandom secureRandom = new SecureRandom();
 	
 	public static void main(String[] args) {
-		int[] testKeys = generateKeys();
-		String testText = "Hello World";
+		int[][] testKeys = generateKeys();
+		String testText = "Some plain text for testing";
+		
+		long start = System.currentTimeMillis();
+		
 		System.out.println(decrypt(encrypt(testText,testKeys),testKeys));
+		
+		long end = System.currentTimeMillis()-start;
+		
+		System.out.println(end);
 	}
 	
-	public static final String decrypt(String text, final int[] keys) {
+	public static final String decrypt(String text, final int[][] keys) {
 		
 		for(int i = 0; i < keys.length; i++) {
-			text = deepDecode(text,keys[i]);
+			for(int j = 0; j < keys[i].length; j++) {
+				text = deepDecode(text,keys[i][j]);
+			}
 		}
 		
 		return text;
 	}
 	
-	public static final String encrypt(String text, final int[] keys) {
+	public static final String encrypt(String text, final int[][] keys) {
 		
 		for(int i = 0; i < keys.length; i++) {
-			text = deepEncode(text,keys[i]);
+			for(int j = 0; j < keys[i].length; j++) {
+				text = deepEncode(text,keys[i][j]);
+			}
 		}
 		
 		return text;
 	}
 	
-	public static final int[] generateKeys() {
-		final int[] keys = new int[1 + (int) secureRandom.nextInt(MAX_KEYS)];
+	public static final int[][] generateKeys() {
+		final int[][] keys = new int[1 + (int) secureRandom.nextInt(MAX_KEYS)][1 + (int) secureRandom.nextInt(MAX_DEEP_FOR_INNER_ARRAYS_OF_KEYS)];
 		
 		for(int i = 0; i < keys.length; i++) {
-			keys[i] = Integer.MIN_VALUE + secureRandom.nextInt(Integer.MAX_VALUE)*2;
+			for(int j = 0; j < keys[i].length; j++) {
+			keys[i][j] = secureRandom.nextInt(Integer.MAX_VALUE);
+			}
 		}
 		
 		return keys;
@@ -54,28 +68,33 @@ public final class AIE {
 		return Integer.MIN_VALUE + random.nextInt(Integer.MAX_VALUE)*2;
 	}
 	
-	private static final String deepEncode(String text, final int key) {
+	private static final String deepEncode(final String text, final int key) {
 		random.setSeed(key);
+		
+		final StringBuilder sb = new StringBuilder(text);
 		
 		for(int i = 0; i < random.nextInt(MAX_DEEP); i++) {
 			random.setSeed(key+i);
-			
-			text = encode(text,generateKey());
-			
+			sb.append(encode(sb.toString(),generateKey()));
+			sb.delete(0, text.length());
 		}
 		
-		return text;
+		return sb.toString();
 	}
 	
-	private static final String deepDecode(String text, final int key) {
+	private static final String deepDecode(final String text, final int key) {
 		random.setSeed(key);
+		
+		final StringBuilder sb = new StringBuilder(text);
 		
 		for(int i = 0; i < random.nextInt(MAX_DEEP); i++) {
 			random.setSeed(key+i);
-			text = decode(text,generateKey());
+
+			sb.append(decode(sb.toString(),generateKey()));
+			sb.delete(0, text.length());
 		}
 		
-		return text;
+		return sb.toString();
 	}
 	
 	private static final String encode(final String text, final int key) {
